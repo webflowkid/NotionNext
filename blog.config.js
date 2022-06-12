826ae0de1cac1cb41b6f2c641f44c7d0079aa290
Webflow Kid <104554198+webflowkid@users.noreply.github.com>
2022-06-12 17:16:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'tenten', // 作者
   BIO: 'Digital Agency - No Code/ Webflow Expert', // 作者简介
-  LINK: 'https://clubmunger.com', // 网站地址
+  LINK: 'https://blog.clubmunger.com', // 网站地址
   KEYWORDS: 'Notion CMS, No Code Agency, Webflow Agency', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
