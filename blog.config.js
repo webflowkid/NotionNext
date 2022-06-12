e2d904304680589863c36f9af1e34c3d2c8d6040
Webflow Kid <104554198+webflowkid@users.noreply.github.com>
2022-06-12 17:19:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -42,11 +42,11 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com',
-  CONTACT_WEIBO: '',
-  CONTACT_TWITTER: '',
-  CONTACT_GITHUB: 'https://github.com/tangly1024',
-  CONTACT_TELEGRAM: '',
+  CONTACT_EMAIL: 'hello@clubmunger.com',
+  CONTACT_WEIBO: 'clubmunger',
+  CONTACT_TWITTER: 'clubmunger',
+  CONTACT_GITHUB: 'https://github.com/webflowkid',
+  CONTACT_TELEGRAM: 'clubmunger',
 
   // 鼠标点击烟花特效
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 鼠标点击烟花特效
